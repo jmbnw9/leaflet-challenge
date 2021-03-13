0bf5870ee35274cfa89a0b289aa833d2d2b1f80a
Janelle <jmbnw9@gmail.com>
2021-03-12 18:00:11 -0600

    working on legend

--- static/js/logic.js
+++ static/js/logic.js
@@ -18,32 +18,61 @@ var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_we
 
 // Perform a GET request to the query URL
 function depth(x) {
-  return x>90 ? "#9f1d18" :
-  x>70 ? "#e1423c":
-  x>50 ? "#de7a26":
-  x>30 ? "#dea826":
-  x>10 ? "#dec726":
-  "#99de26";
+  return x > 90 ? "#9f1d18" :
+    x > 70 ? "#e1423c" :
+      x > 50 ? "#de7a26" :
+        x > 30 ? "#dea826" :
+          x > 10 ? "#dec726" :
+            "#99de26";
 }
 
-d3.json(queryUrl, function(data) {
+d3.json(queryUrl, function (data) {
   // Creating a geoJSON layer with the retrieved data
   L.geoJson(data, {
-    onEachFeature: function(feature, layer) {
+    onEachFeature: function (feature, layer) {
       layer.bindPopup("<h1>" + feature.properties.mag + "</h1> <hr> <h2>" + feature.properties.place + "</h2>");
 
     },
     pointToLayer: function (feature, latlng) {
       return L.circleMarker(latlng, {
-        radius: feature.properties.mag *4,
+        radius: feature.properties.mag * 4,
         fillColor: depth(feature.geometry.coordinates[2]),
         color: "#040401",
         weight: 1,
         opacity: 1,
         fillOpacity: 0.8
-    });
-  }
+      });
+    }
 
   }).addTo(myMap);
-});
 
+  // Set up the legend
+  var legend = L.control({ position: 'bottomright' });
+  legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend');
+    mags = [0, 1, 2, 3, 4, 5],
+      labels = [];
+   
+    for (var i = 0; i < mag.length; i++) {
+
+      div.innerHTML +=
+        labels.push(
+          '<i style="background:' + getColor(mags[i]) + '"></i> ' +
+          (mags[i] ? mags[i] : '+'));
+
+    }
+
+    function getColor(depth) {
+      return depth > 5 ? "#9f1d18" :
+        depth > 4 ? "#e1423c" :
+          depth > 3 ? "#de7a26" :
+            depth > 2 ? "#dea826" :
+              depth > 1 ? "#dec726" :
+                "#99de26";
+      return div;
+    };
+    // Adding legend to the map
+    legend.addTo(myMap);
+  };
+});
